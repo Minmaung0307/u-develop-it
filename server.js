9ee053d26df7d93adaceb91484a2f1d1b1f72977
Min Maung <minmaung0307@gmail.com>
2022-09-13 12:25:19 -0400

    add parties

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mysql = require("mysql2");
 const inputCheck = require("./utils/inputCheck");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 // Express middleware
